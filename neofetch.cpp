f408354282e84b67468a2f57101a84af2421ba1f
sutirtha0505 <tirtharajkarmakarinlinux@gmail.com>
2024-08-01 12:38:36 +0530

    first commit

---
+++ neofetch.cpp
@@ -0,0 +1,19 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+  // Wait for the system to recognize the device
+  DigiKeyboard.delay(10000);
+
+  // Open the terminal using Ctrl + Alt + T
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(KEY_T, MOD_CONTROL_LEFT | MOD_ALT_LEFT); // Ctrl + Alt + T
+  DigiKeyboard.delay(1000);
+
+  // Type and execute the neofetch command
+  DigiKeyboard.print("neofetch");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+}
+
+void loop() {
+  // nothing to do here
+}
